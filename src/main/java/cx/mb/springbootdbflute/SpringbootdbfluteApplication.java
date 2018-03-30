73598c63dcbe796b7135103c8df76efa02a4fad9
Toshiaki Ito <evolution@1024kb.cx>
2018-03-31 08:27:05 +0900

    init.

---
+++ src/main/java/cx/mb/springbootdbflute/SpringbootdbfluteApplication.java
@@ -0,0 +1,12 @@
+package cx.mb.springbootdbflute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdbfluteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdbfluteApplication.class, args);
+    }
+}
---
+++ src/test/java/cx/mb/springbootdbflute/SpringbootdbfluteApplicationTests.java
@@ -0,0 +1,16 @@
+package cx.mb.springbootdbflute;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootdbfluteApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
